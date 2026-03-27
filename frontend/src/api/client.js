d31fb5327cb671a70719e72d3303f7503e05d65e
sanchit_sehgal <sanchitsehgal16@gmail.com>
2026-03-27 20:18:59 +0530

    inital commit

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000'
+const API_BASE = (
+  import.meta.env.VITE_API_BASE_URL
+  || import.meta.env.VITE_API_BASE
+  || 'http://127.0.0.1:8000'
+).replace(/\/+$/, '')
 
 export const api = axios.create({
   baseURL: API_BASE,
